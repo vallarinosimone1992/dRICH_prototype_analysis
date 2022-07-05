e6103294cffae8eeb6a51fdb7e144cdb2db17c8b
vallarinosimone1992 <simonevallarino92@gmail.com>
2022-07-05 09:56:45 +0200

    Try CMake compiling

---
+++ sw/build/CMakeFiles/dRICH_lib.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dRICH_lib.dir/lib/MAPMTposition.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/MAPMTposition.C.o.d"
+  "CMakeFiles/dRICH_lib.dir/lib/dRICH.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/dRICH.C.o.d"
+  "CMakeFiles/dRICH_lib.dir/lib/fillMAPS.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/fillMAPS.C.o.d"
+  "CMakeFiles/dRICH_lib.dir/lib/getChannel.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/getChannel.C.o.d"
+  "CMakeFiles/dRICH_lib.dir/lib/handleFile.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/handleFile.C.o.d"
+  "CMakeFiles/dRICH_lib.dir/lib/integrate.C.o"
+  "CMakeFiles/dRICH_lib.dir/lib/integrate.C.o.d"
+  "libdRICH_lib.a"
+  "libdRICH_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dRICH_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
