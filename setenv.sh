8ed6a83959145f2d59db1da42843c97db0659429
vallarinosimone1992 <simonevallarino92@gmail.com>
2022-07-07 16:03:59 +0200

    Adding setenv

---
+++ setenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "${DRICH_SUITE}" ]]; then
+  echo "You have to set the environment variables DRICH_SUITE as the upstream directory"
+  exit
+fi
+
+echo "The env variable DRICH_SUITE is $DRICH_SUITE"
+
+mkdir -p $DRICH_SUITE/DATA/dRICH_DATA
+mkdir -p $DRICH_SUITE/DATA/GEM_DATA
+mkdir -p $DRICH_SUITE/DATA/header_csv
+mkdir -p $DRICH_SUITE/processed_data/integrated_dRICH_GEM_data
+mkdir -p $DRICH_SUITE/output
+
+
